0fc1cbd493058727144997eb1ce8fc932d7c1372
bogdan <bogdan.stefan3@gmail.com>
2015-03-16 21:08:22 +0200

    Add MapSearch basic class template

---
+++ MapSearch.cpp
@@ -0,0 +1,32 @@
+#include "MapSearch.h"
+
+MapSearch::MapSearch()
+{
+	// nothing to do
+}
+
+MapSearch::~MapSearch()
+{
+	// nothing to do
+}
+
+MapSearch& MapSearch::get_instance()
+{
+	static MapSearch instance;
+	return instance;
+}
+
+unsigned MapSearch::get_closest_front(const unsigned& region)
+{
+	return front_distances[region];
+}
+
+unsigned MapSearch::get_min_distance(const unsigned& fromRegion, const unsigned& toRegion){
+	return distances[fromRegion][toRegion];
+}
+
+std::vector<unsigned> MapSearch::road_to(const unsigned& fromRegion, const unsigned& toRegion) {
+	//TODO Dijkstra, maybe?
+	std::vector<unsigned> result;
+	return result;
+}
