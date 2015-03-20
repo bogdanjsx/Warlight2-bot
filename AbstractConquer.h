781fd25964e968389100ead74220f9e9dba98176
rotalex <nucleul.tocilarilor@gmail.com>
2015-03-20 19:49:04 +0000

    add the interface for the main actions

---
+++ AbstractConquer.h
@@ -0,0 +1,24 @@
+#ifndef ABSTRACTCONQUER_H
+#define ABSTRACTCONQUER_H
+
+#include <vector>
+#include <cstring>
+#include "Action.h"
+
+/*
+	This class will represent the interface for the objects that will deal with the conquer phase
+	It will be instantiated with a fancy-algorithm object that will compute the best way the bot
+	can develop and conquer a superregion.
+*/
+
+class AbstractConquer : public virtual Action {
+public:
+	// no parameters
+	std::string getType() const { return type; }
+	std::vector<Project> getProjects();
+
+protected:
+	std::string type = "conquer";
+};
+
+#endif
